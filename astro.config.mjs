70c50f900f5162368514d94ce9e930d70a08b635
TheAdmanCross <the.adman.cross@gmail.com>
2026-04-09 15:57:21 +1000

    chore: configure netlify cms login

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,9 +1,11 @@
 import { defineConfig } from "astro/config";
 import react from "@astrojs/react";
 
+const isGitHubPages = process.env.GITHUB_ACTIONS === "true";
+
 export default defineConfig({
-  site: "https://theadmancross.github.io",
-  base: "/twm",
+  site: isGitHubPages ? "https://theadmancross.github.io" : "https://twm.netlify.app",
+  base: isGitHubPages ? "/twm" : "/",
   integrations: [react()],
   output: "static"
 });
